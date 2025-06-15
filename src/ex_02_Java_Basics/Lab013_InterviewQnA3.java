5ccd567a9e5f5d5a08052e96909036635a59f02c
Abhinav Choudhary <158158286+abhi03ongithub@users.noreply.github.com>
2025-06-15 17:38:20 +0530

    New Changes 1

--- src/ex_02_Java_Basics/Lab013_InterviewQnA3.java
+++ src/ex_02_Java_Basics/Lab013_InterviewQnA3.java
@@ -4,7 +4,7 @@ public class Lab013_InterviewQnA3 {
     public static void main(String[] args)
     {
 
-        int a = 10;
+
         {
             int b = 10;
             System.out.println(b);
