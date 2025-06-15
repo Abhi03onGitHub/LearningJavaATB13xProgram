0992781e0ad522013888493919da2917b5788300
Abhi03onGitHub <er.abhinav.3@gmail.com>
2025-06-15 15:03:19 +0530

    First commit

--- src/ex_01_Java_Basic/Lab002_NoMainMethod.java
+++ src/ex_01_Java_Basic/Lab002_NoMainMethod.java
@@ -1,6 +1,9 @@
 package ex_01_Java_Basic;
 
 public class Lab002_NoMainMethod {
-
+public static void main(String[] args){
+    System.out.println("No Main Method Check");
+}
+   // System.out.println("No Main Method Check"); this will not run outside main method
 
 }
