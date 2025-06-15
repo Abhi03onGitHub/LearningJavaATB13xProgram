eb7b278212b3bbbf08724344d1f76d7e82635534
Abhinav Choudhary <158158286+Abhi03onGitHub@users.noreply.github.com>
2025-06-15 15:25:31 +0530

    Update Lab002_ShortcutHW.java

--- src/ex_01_Java_Basic/Lab002_ShortcutHW.java
+++ src/ex_01_Java_Basic/Lab002_ShortcutHW.java
@@ -4,6 +4,7 @@ public class Lab002_ShortcutHW {
     public static void main(String[] args)
     {
         System.out.println("Hello Test");
+        System.out.println("New Changes Check");
     }
     public static void main(int args)
     {
